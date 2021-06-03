5602f630773962ae9f87b44a2b93cf6cfa76c663
Jesse Brown <Brownjesse8895@yahoo.com>
2021-06-02 19:32:18 -0500

    Using the Array Find Method

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+function superbowlWin(arrayOfObjects){
+    let winners = arrayOfObjects.find(record => record.result === "W")
+    return winners ? winners.year : undefined
+    // let winners = arrayOfObjects.find(function(record) { 
+    //     return record.result === "W"
+    // })
+    // if (winners){
+    //     return winners.year 
+    // }
+    // else{
+    //     return undefined
+    // }
+
+}
