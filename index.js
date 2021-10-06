a812eb753419006c5703465d06b53e779c2228f8
Jesse Brown <Brownjesse8895@yahoo.com>
2021-10-06 16:14:05 -0500

    Using the Array Find Method
    Created a variable called superBowlWin and assigned it to an arrow function that takes in one parameter(array) within the arrow function we careate a variable called year and assign it to the rcords and iterate through it to try and find if the given team has ever won a superbowl. WWe then use the ternary operator and if through the iteration there is a value that returns true that the team has won it returns that year. If the team has never won then it will return undefined

--- index.js
+++ index.js
@@ -1,15 +1,65 @@
-
-function superbowlWin(arrayOfObjects){
-    let winners = arrayOfObjects.find(record => record.result === "W")
-    return winners ? winners.year : undefined
-    // let winners = arrayOfObjects.find(function(record) { 
-    //     return record.result === "W"
-    // })
-    // if (winners){
-    //     return winners.year 
-    // }
-    // else{
-    //     return undefined
-    // }
-
+const superBowlWin = record => {
+    const year = record.find(array => array.result === "W")
+    return !!year ? year.year : undefined
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+// function superbowlWin(arrayOfObjects){
+//      let winners = arrayOfObjects.find(record => record.result === "W")
+//     return winners ? winners.year : undefined
+//     let winners = arrayOfObjects.find(function(record) { 
+//         return record.result === "W"
+//      })
+//      if (winners){
+//          return winners.year 
+//      }
+//     else{
+//         return undefined
+//      }
+
+//}
